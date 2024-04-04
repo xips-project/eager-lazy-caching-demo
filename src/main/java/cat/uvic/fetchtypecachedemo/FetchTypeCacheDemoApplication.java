83b31397708d91fba5880c525ed647b896d978b3
afcasco <7701272+afcasco@users.noreply.github.com>
2024-04-04 13:00:45 +0200

    switch default cache manager to caffeine

--- src/main/java/cat/uvic/fetchtypecachedemo/FetchTypeCacheDemoApplication.java
+++ src/main/java/cat/uvic/fetchtypecachedemo/FetchTypeCacheDemoApplication.java
@@ -1,10 +1,19 @@
 package cat.uvic.fetchtypecachedemo;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@EnableCaching
 public class FetchTypeCacheDemoApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(FetchTypeCacheDemoApplication.class, args);
